f981be1269ee532bf129c7a15c2621a13267ff29
jeparson <jerod.parsons@nist.gov>
2015-05-04 11:08:13 -0700

    Dynamic UI demoing/testing

---
+++ server.R
@@ -0,0 +1,31 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://www.rstudio.com/shiny/
+#
+functiongoeshere<-function(fnin=c(redvec,purvec,bluevec,orangevec,greenvec)){dothefunction()}
+dothefunction<-function(fnin=c(redvec,purvec,bluevec,orangevec,greenvec)){sum(fnin)}
+library(shiny)
+
+shinyServer(function(input, output) {
+    output$ui<-renderUI({
+        if(is.null(input$input_type))
+            return()
+        switch(input$input_type,
+        "Red" = checkboxGroupInput("redvec","ListOfReds",choices=c("All","A","B","C","D")),
+        "Purple" = checkboxGroupInput("purvec","ListOfPurps",choices=c("All","P1","B","C","D")),
+        "Blue" = checkboxGroupInput("bluevec","ListOfBlues",choices=c("All","B1","B","C","D")),
+        "Orange" = checkboxGroupInput("orangevec","ListOfOranges",choices=c("All","O1","B","C","D")),
+        "Green" = checkboxGroupInput("greenvec","ListOfGreens",choices=c("All","G1","B","C","D"))
+)
+    })
+
+
+    output$resulttable<-renderDataTable({dothefunction(c(redvec,purvec,bluevec,orangevec,greenvec))})
+
+}
+
+  )
+
+
---
+++ ui.R
@@ -0,0 +1,32 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://www.rstudio.com/shiny/
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Dynamic UI"),
+fluidRow(
+    column(3, wellPanel(
+        selectInput("input_type","Color",c("Red","Purple","Blue","Orange","Green")))),
+    column(3, wellPanel(uiOutput("ui"))),
+    column(3, tags$p("Best_Option:")),
+    tableOutput(outputId = "functionresult")
+    )
+  #want a switch between colors and the various names to pop up
+
+
+)
+)
+  # Show a plot of the generated distribution
+
+
+
+##I don't really like the clutteredness of this interface:  You can apparently do dynamic UIs so i could do an "allred" yes/no that would expand if no.
+
+###highest_current_color dropdown?
