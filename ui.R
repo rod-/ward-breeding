458a9c17596f65373b4366ced8805ec4782745fc
rod- <jerparsons@gmail.com>
2015-05-08 16:01:15 -0700

    mch

--- ui.R
+++ ui.R
@@ -8,12 +8,12 @@ shinyUI(fluidPage(
       checkboxGroupInput("fullgroups","Check your completed colors",c("Red","Purple","Blue","Orange","Green")))),
       column(3, wellPanel(uiOutput("ui"))),
         fluidRow(    column(3, wellPanel(
-            selectInput('input_types', 'Choose Partial Colors', choices=c("Purple","Blue","Red","Orange","Green"), multiple=TRUE,selected = c("Purple","Blue"), selectize=TRUE))),
-            column(3,wellPanel(checkboxGroupInput('show_vars', 'Columns to show:', names(DragonID), selected = '')))
+            selectInput('input_types', 'Choose Partial Colors', choices=c("Purple","Blue","Red","Orange","Green"), multiple=TRUE,selected = c("Purple","Blue"), selectize=TRUE)))#,
+#            column(3,wellPanel(checkboxGroupInput('show_vars', 'Columns to show:', names(DragonID), selected = '')))
 ),
 helpText("The best pairing with the currently-selected set of dragons is the top line in the table below"),
 fluidRow(column(12,wellPanel(dataTableOutput("resulttable")))),
-fluidRow(column(12,wellPanel(dataTableOutput("dragstat")))),
+#fluidRow(column(12,wellPanel(dataTableOutput("dragstat")))),
          helpText("Disclaimer:  Not associated with Pocket Gems. Images are property of Pocket Gems")
 
 
